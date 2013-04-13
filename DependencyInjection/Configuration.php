465a9b101796e3b94cdf0a29b03f8c56887b2543
dantleech <dan.t.leech@gmail.com>
2013-04-13 22:57:04 +0100

    Updated composer again

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DTL\Bundle\DistanceTimeBundle\DependencyInjection;
+namespace DTL\Bundle\TimeDistanceTBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
