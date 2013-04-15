8e861137ff3c83e1c66542d987f643de0a05893c
dantleech <dan.t.leech@gmail.com>
2013-04-15 11:55:19 +0100

    Changed signature of `format_distance` to enable specifying "from" unit
    Allow overwriting of default base unit

--- Twig/TimeDistanceExtension.php
+++ Twig/TimeDistanceExtension.php
@@ -23,21 +23,25 @@ class TimeDistanceExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'seconds_as_stopwatch' => new \Twig_Filter_Method($this, 'secondsAsStopwatch'),
+            'seconds_as_stopwatch' => new \Twig_Filter_Method($this, 'secondsToStopwatch'),
             'format_distance' => new \Twig_Filter_Method($this, 'formatDistance'),
             'average_pace' => new \Twig_Filter_Method($this, 'averagePace'),
             'average_speed' => new \Twig_Filter_Method($this, 'averageSpeed'),
         );
     }
 
-    public function secondsAsStopwatch($seconds)
+    public function secondsToStopwatch($seconds)
     {
-        return $this->tdh->secondsAsStopwatch($seconds);
+        return $this->tdh->secondsToStopwatch($seconds);
     }
 
-    public function formatDistance($distance, $to = 'km', $precision = 2)
+    public function formatDistance($distance, $to = 'km', $from = null, $precision = 2)
     {
-        return $this->tdh->formatDistance($distance, $this->baseDistanceUnit, $to, $precision);
+        if (null === $from) {
+            $from = $this->baseDistanceUnit;
+        }
+
+        return $this->tdh->formatDistance($distance, $from, $to, $precision);
     }
 
     public function averagePace($seconds, $distance, $to = 'km')
